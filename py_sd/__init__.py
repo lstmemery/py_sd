99d26de10260c4449072e9d9bcda1e9a1160f126
Matthew Emery <atriumcomplex@gmail.com>
2016-11-22 19:18:48 -0800

    Added standard error with documentation

--- py_sd/__init__.py
+++ py_sd/__init__.py
@@ -4,3 +4,14 @@ def standard_deviation(x):
     ssq = sum((x_i-mean)**2 for x_i in x)
     stdev = (ssq/n)**0.5
     return(stdev)
+
+"""
+Calculates the standard error.
+
+:param x: an array of numbers
+:return: The standard error.
+
+>>> standard_error([1, 2, 3])
+0.47140452079103173
+"""
+standard_error = lambda x: standard_deviation(x)/len(x)**0.5
