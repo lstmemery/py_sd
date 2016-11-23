22ba94972a668f732a4ead9766f800cd8baa4737
David Laing <david.laing@alumni.ubc.ca>
2016-11-22 18:23:11 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_sd',
+    version='0.1',
+    description='Standard deviation of some numbers.',
+    url='https://github.com/laingdk/py_sd.git',
+    author="D. Laing",
+    author_email="davidkendalllaing@gmail.com",
+    license='MIT',
+    packages=['py_sd'],
+    zip_safe=False)
